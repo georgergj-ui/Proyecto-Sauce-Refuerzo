c2ed5fa094f5dd6f9c9ecc8b4ae8fb48b3b40c4e
georgergj-ui <georgergj@gmail.com>
2026-07-02 04:08:43 -0600

    Login and Inventory
    Login and Inventory pages are ready on POM

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,10 +1,46 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from .base_page import BasePage
-
 
 class Inventorypage (BasePage):
     CART = (By.ID, "shopping_cart_container")
+    HAM = (By.ID, "react-burger-menu-btn")
+    SUBMENU = (By.XPATH, "//nav[@class='bm-item-list']//a")
+    PRODUCTS = (By.XPATH, "//div[@class='inventory_item']")
+    FILTER =(By.CLASS_NAME, "product_sort_container")
+    PRODUCT_NAMES = (By.CLASS_NAME, "inventory_item_name")
+    ADD_CART = (By.CSS_SELECTOR, ".btn.btn_primary.btn_small.btn_inventory")
+    REMOVE_CART =(By.CSS_SELECTOR, ".btn.btn_secondary.btn_small.btn_inventory")
+    CART_BADGE =(By.CLASS_NAME, "shopping_cart_badge")
 
     def cart_icon_validate(self):
         return self.wait_for_element(self.CART)
+    
+    def hamburguer_icon_validate(self):
+        return self.wait_for_element(self.HAM)
+    
+    def hamburger_sub_menu_validate(self):
+        return self.driver.find_elements(*self.SUBMENU)
+    
+    def inventory_products(self):
+        return self.driver.find_elements(*self.PRODUCTS)
+    
+    def sort_filter(self, option):
+       dropdown = self.wait_for_element(self.FILTER)
+       Select(dropdown).select_by_visible_text(option)
+
+    def product_names(self):
+        return self.driver.find_elements(*self.PRODUCT_NAMES)
+    
+    def add_cart_buttons(self):
+        return self.driver.find_elements(*self.ADD_CART)
+    
+    def remove_cart_buttons(self):
+        return self.driver.find_elements(*self.REMOVE_CART)
+    
+    def cart_badge(self):
+       return self.wait_for_element(self.CART_BADGE)
+    
+    def cart_badges(self):
+        return self.driver.find_elements(*self.CART_BADGE)
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -3,9 +3,94 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
-@pytest.mark.inventory
+
 def test_inventory_cart_icon_loaded(login_page, inventory_page):
     login_page.navigate_login()
-    login_page.login("standard_user", "secret_sauce" )
+    login_page.login("standard_user", "secret_sauce")
     assert inventory_page.cart_icon_validate().is_displayed()
+
+
+def test_inventory_hamburguer_nav_loaded(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    assert inventory_page.hamburguer_icon_validate().is_displayed()
+
+def test_inventory_hamburger_sub_menu_loaded(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    inventory_page.hamburguer_icon_validate().click()
+    options = inventory_page.hamburger_sub_menu_validate()
+
+    expected_options = [
+        "All Items",
+        "About",
+        "Logout",
+        "Reset App State"
+    ] 
+    
+    actual_options = []
+
+    for option in options:
+       actual_options.append(option.get_attribute("textContent"))
+
+    assert actual_options == expected_options, "La lista no es la correcta, talvez un texto no es el esperado"
+
+
+def test_inventory_has_six_products(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    products: list[WebElement] = inventory_page.inventory_products()
+    assert len(products) == 6
+    for product in products:
+        assert product.is_displayed()
+
+
+def test_inventory_sort_H_to_L(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    inventory_page.sort_filter("Price (high to low)")
+    first_product = inventory_page.product_names()[0]
+    assert first_product.text == "Sauce Labs Fleece Jacket"
+
+def test_inventory_sort_L_to_H(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    inventory_page.sort_filter("Price (low to high)")
+    first_product = inventory_page.product_names()[0]
+    assert first_product.text == "Sauce Labs Onesie"
+
+def test_inventory_sort_A_to_Z(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    inventory_page.sort_filter("Name (A to Z)")
+    first_product = inventory_page.product_names()[0]
+    assert first_product.text == "Sauce Labs Backpack"
+
+def test_inventory_sort_Z_to_A(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    inventory_page.sort_filter("Name (Z to A)")
+    first_product = inventory_page.product_names()[0]
+    assert first_product.text == "Test.allTheThings() T-Shirt (Red)"
+
+def test_inventory_add_product_update_cart_badge(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    buttons = inventory_page.add_cart_buttons()
+    buttons[0].click()
+    buttons[1].click()
+    badge = inventory_page.cart_badge()
+    assert badge.is_displayed()
+    assert badge.text == "2", "Se estan agregando un numero incorrecto de Productos al carrito"
+@pytest.mark.inventory
+def test_inventory_add_product_and_remove_from_inventory(login_page, inventory_page):
+    login_page.navigate_login()
+    login_page.login("standard_user", "secret_sauce")
+    buttons = inventory_page.add_cart_buttons()
+    buttons[0].click()
+    buttons_remove = inventory_page.remove_cart_buttons()
+    buttons_remove[0].click()
+    badge = inventory_page.cart_badges()
+    assert len(badge) == 0
